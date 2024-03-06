89be822abd9e5bfb7ec19a1c098708d8ee1d4ce1
Benjamin Goihman <bengoihman@gmail.com>
2024-03-05 22:07:55 -0500

    end of day 1 prob, just releanrign basic syntax

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
-
+from django.urls import reverse
+from products.models import Product
 # Create your tests here.
+
+class BasicTest(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        Product.objects.create(title="ti")
+    def test_url_exist(self):
+        url = reverse("home")
+        self.assertEqual(self.client.get(url).status_code,200)
+        print(self.client.get(url).content)
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,12 @@
 from django.http import JsonResponse
-
+from products.models import Product
 # Create your views here.
 
 def api_home(requests):
-    return JsonResponse({"test":"hi"})
+    model_data = Product.objects.all().order_by("?").first()
+    data={}
+    if model_data:
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
+    return JsonResponse(data)
