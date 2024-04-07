71d6b855afbe6c75e540421c4ba8956d511c9982
Benjamin Goihman <bengoihman@gmail.com>
2024-04-06 20:07:02 -0400

    fixe baseurl

--- backend/tempfront/ailingo-frontend/src/utils/auth.js
+++ backend/tempfront/ailingo-frontend/src/utils/auth.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import axiosInstance from './axiosInstance';
 
-const BASE_URL = 'http://localhost:8000/api';
+const BASE_URL = 'https://ailingo-production.up.railway.app/api';
 
 export const loginUser = async (email, password) => {
   try {
