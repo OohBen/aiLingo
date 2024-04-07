e4efd91f05aa0ab587aa4f19782a021019ce830e
Benjamin Goihman <bengoihman@gmail.com>
2024-04-06 20:21:09 -0400

    please work allwoed hosts pelae

--- backend/aiLingo/aiLingo/settings.py
+++ backend/aiLingo/aiLingo/settings.py
@@ -19,7 +19,7 @@ load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1", "t")
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
+ALLOWED_HOSTS = ["*"]
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
