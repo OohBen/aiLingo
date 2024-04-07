0a28f41a4d6702d12d953c4c210911b7958341f1
Benjamin Goihman <bengoihman@gmail.com>
2024-04-06 22:00:19 -0400

    del cacghe

--- backend/aiLingo/aiLingo/settings.py
+++ backend/aiLingo/aiLingo/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
+ 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-0b3)jdf!ev5a)r4bloar5gpgdi5%7kvzdn)g2$bmdxs&ed$hwp"
 
