072eddc7538a476ccdf3a7c54ceca75860f0aa29
Benjamin Goihman <bengoihman@gmail.com>
2024-05-06 19:33:03 -0400

    try 3 for no more eslint

--- frontend/ailingo/next.config.mjs
+++ frontend/ailingo/next.config.mjs
@@ -3,20 +3,6 @@ const nextConfig = {
 //disaslbe eslint
 eslint: {
   ignoreDuringBuilds: true,
-},
-  reactStrictMode: true,
-  images: {
-    domains: ['res.cloudinary.com'],
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: securityHeaders,
-      },
-    ];
-  },
-
-
+}
 };
 export default nextConfig;
